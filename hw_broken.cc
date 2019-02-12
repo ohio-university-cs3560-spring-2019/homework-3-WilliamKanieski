f1c7985c84481575c99030bcfd63eaa0b00c54a8
unknown <wk142016@ohio.edu>
2019-02-11 20:03:28 -0500

    My first submission for HW3: has function for standard deviation and utilizes doxygen for documentation.

--- hw_broken.cc
+++ hw_broken.cc
@@ -1,7 +1,16 @@
+/**
+* \file
+*/
+
 #include <iostream>
 #include <cmath>
 
-
+/**
+* \brief calculates standard deviation
+* \param a a dynamic array of integers used to store the data set
+* \param n the size of the dynamic array used to store the data set
+* \return the standard deviation of the integers in the dynamic array expressed as a double
+*/
 double deviation(int* a, int n)
 {
 	double sum = 0.0;

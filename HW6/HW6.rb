2c15181f823c7a84522c500ec8c4d31956349c9b
unknown <wk142016@ohio.edu>
2019-02-25 23:23:23 -0500

    I revised the code for HW6 a bit for Bonus 2.

--- HW6/HW6.rb
+++ HW6/HW6.rb
@@ -1,7 +1,13 @@
 input = gets 
 
 input = input.gsub(/\bf/, "gh")
+input = input.gsub(/\bF/, "Gh")
 input = input.gsub(/sh\b/, "ti")
+input = input.gsub(/Sh\b/, "Ti")
+input = input.gsub(/sH\b/, "tI")
+input = input.gsub(/SH\b/, "TI")
 input = input.gsub(/\Bi\B/, "o")
+input = input.gsub(/\BI\B/, "O")
+
 
 puts input
